52becb6cabf220455f1cbd8f16ebc9b03fa4c876
groot <vijaysingh88@hotmail.com>
2021-02-02 20:09:37 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+link to tutorials...
+
+https://nextjs.org/learn/basics/dynamic-routes/implement-getstaticprops
---
+++ pages/api/hello.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+    res.status(200).json({ text: 'Hello' })
+  }
+  
+  // try to find why this route link is not working ,,
+  //it would be helpful in future to use it.
+  
